d4a36fec5f72f5d5f0fb88b1ea95ea2fc653117d
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-09-22 16:08:38 +0530

    Add TailwindCSS to frontend

--- weather-app-frontend/src/components/Header.jsx
+++ weather-app-frontend/src/components/Header.jsx
@@ -1,19 +1,23 @@
 import PropTypes from 'prop-types';
 import sunCloudIcon from '../img/sun_cloud.svg';
-import '../css/main.css';
+import '../index.css';
 
 const Header = ({
     place, weather, weekday, date
 }) => {
     return (
-        <div className="header">
-            <div className="header-weather">
-                <img src={sunCloudIcon} alt="cloudy" width={100}></img>
-                <h2>{weather}</h2>
-                <h3>{place}</h3>
+        <div className="px-24 py-10 flex flex-row-reverse justify-between items-center">
+            <div className="flex items-center">
+                <div>
+                    <img src={sunCloudIcon} alt="cloudy" width={100}></img>
+                </div>
+                <div>
+                    <h2 className="text-2xl">{weather}</h2>
+                    <h3 className="text-xl">{place}</h3>
+                </div>
             </div>
 
-            <div className="header-datetime">
+            <div className="text-lg">
                 <h3>{weekday}</h3>
                 <h3>{date}</h3>
             </div>
