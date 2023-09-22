05284c9bddb9961d5c8b85b3d0de9fdc928e2850
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-09-22 12:21:06 +0530

    Connect backend to the frontend

--- weather-app-frontend/src/App.jsx
+++ weather-app-frontend/src/App.jsx
@@ -1,9 +1,38 @@
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
+import weatherService from './services/weather';
 
 const App = () => {
+  const [place, setPlace] = useState('');
+  const [weather, setWeather] = useState('');
+  const [weekday, setWeekday] = useState('');
+  const [date, setDate] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const weatherData = await weatherService.getAll();
+
+        const fullDateString = weatherData.dateTime.date;
+        const dateParts = fullDateString.split(',').map(part => part.trim());
+        const today = dateParts[0];
+        const todayDate = `${dateParts[1]}, ${dateParts[2]}`;
+
+        setPlace(`${weatherData.city}, ${weatherData.country}`);
+        setWeather(weatherData.weather.condition);
+        setWeekday(today);
+        setDate(todayDate);
+      } catch (error) {
+        console.error("error fetching weather data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header place={place} weather={weather} weekday={weekday} date={date} />
     </>
   );
 };
--- weather-app-frontend/src/components/Header.jsx
+++ weather-app-frontend/src/components/Header.jsx
@@ -1,21 +1,31 @@
+import PropTypes from 'prop-types';
 import sunCloudIcon from '../img/sun_cloud.svg';
 import '../css/main.css';
 
-const Header = () => {
+const Header = ({
+    place, weather, weekday, date
+}) => {
     return (
         <div className="header">
             <div className="header-weather">
                 <img src={sunCloudIcon} alt="cloudy" width={100}></img>
-                <h2>Cloudy</h2>
-                <h3>Puducherry, India</h3>
+                <h2>{weather}</h2>
+                <h3>{place}</h3>
             </div>
 
             <div className="header-datetime">
-                <h3>Friday</h3>
-                <h3>22 September, 2023</h3>
+                <h3>{weekday}</h3>
+                <h3>{date}</h3>
             </div>
         </div>
     );
+};
+
+Header.propTypes = {
+    place: PropTypes.string.isRequired,
+    weather: PropTypes.string.isRequired,
+    weekday: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired
 };
 
 export default Header;
