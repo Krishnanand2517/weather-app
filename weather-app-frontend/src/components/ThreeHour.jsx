cb695d26494c2256fa41eb392c921429120cb31f
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-09-27 17:01:12 +0530

    Add an info icon to show clarification message

--- weather-app-frontend/src/components/ThreeHour.jsx
+++ weather-app-frontend/src/components/ThreeHour.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 import ThreeHourCard from './ThreeHourCard';
+import Info from './Info';
 
 const ThreeHour = ({
     dateArray, timeArray, iconArray, tempArray
@@ -38,7 +39,10 @@ const ThreeHour = ({
 
     return (
         <div className="my-10">
-            <h2 className="mx-12 md:mx-36 lg:mx-56 2xl:mx-96 text-2xl lg:text-3xl font-bold">24-hour Forecast</h2>
+            <h2 className="mx-12 md:mx-36 lg:mx-56 2xl:mx-96 text-2xl lg:text-3xl font-bold">
+                <Info />
+                24-hour Forecast
+            </h2>
             <div className="px-12 md:px-36 lg:px-56 2xl:px-96 py-5 flex items-center justify-center relative">
                 <div className="absolute left-0 top-0 w-6 sm:w-20 ml-12 md:ml-36 lg:ml-56 2xl:ml-96 h-full bg-gradient-to-r from-sky-light to-transparent transition-opacity duration-800" id="left-gradient"></div>
                 <div className="absolute right-0 top-0 w-12 sm:w-28 mr-12 md:mr-36 lg:mr-56 2xl:mr-96 h-full bg-gradient-to-l from-sky-light to-transparent transition-opacity duration-800" id="right-gradient"></div>
